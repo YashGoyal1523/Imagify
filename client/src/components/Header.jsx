da016c2bb1aee306cbb96a7d91cb6a993e1eb934
YashGoyal1523 <yashgoyal1523@gmail.com>
2025-07-12 23:00:31 +0530

    Complete

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,12 +18,12 @@ const Header = () => {
     }
 
   return ( 
-    <div className='flex flex-col justify-center items-center text-center my-12'>
+    <div className='flex flex-col justify-center items-center text-center my-20'>
         <div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'>
             <p>Best text to image generator</p>
             <img src={assets.star_icon} alt="" />
         </div>
-        <h1 className='text-4xl max-w-[300px] sm:text-6xl sm:max-w-[590px] mx-auto mt-6 text-center'>Turn text into <span className='text-blue-600'>image</span>, in seconds.</h1>
+        <h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>Turn text into <span className='text-blue-600'>image</span>, in seconds.</h1>
         <p className='text-center max-w-xl mx-auto mt-5'>Unleash your creativity with AI . Turn your imagination into visual art in seconds - just type, and see the magic happen...</p>
         <button onClick={onClickHandler} className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full cursor-pointer hover:scale-[1.02]'>Generate Images <img className='h-6' src={assets.star_group} alt="" /></button>
 
